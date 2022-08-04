30fd097ca9fae20ba91227a98bafa27c22f3bc1a
dr-Maker <contacto@jonathanvenegas.cl>
2022-08-03 23:10:18 -0400

    fist commt starting whit next app

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+const { redirect } = require("next/dist/server/api-utils");
+
+// module.exports = nextConfig
+
+module.exports = {
+  reactStricMode: true,
+  env: {
+    customKey : 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirect(){
+    return[
+      {
+        source: '/',
+        destination: '/',
+        permanent: true,
+
+      }
+    ]
+  }
+}
